a46f39a6fc226aee192a92ecadab243d8cc8286d
Mehmed Jusic <mehmedjusic@gmail.com>
2024-02-12 23:00:38 +0100

    Updated gitignore

--- backend/src/modules/user/dto/create-user.dto.ts
+++ backend/src/modules/user/dto/create-user.dto.ts
@@ -7,12 +7,12 @@
 //     IsString,
 //     Matches,
 //     MinLength,
-//   } from 'class-validator';
+// } from 'class-validator';
 
-//   const passwordRegEx =
+// const passwordRegEx =
 //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
 
-//   export class CreateUserDto {
+// export class CreateUserDto {
 //     @IsString()
 //     @MinLength(2, { message: 'Name must have atleast 2 characters.' })
 //     @IsNotEmpty()
@@ -21,7 +21,7 @@
 //     @IsNotEmpty()
 //     @MinLength(3, { message: 'Username must have atleast 3 characters.' })
 //     @IsAlphanumeric(null, {
-//       message: 'Username does not allow other than alpha numeric chars.',
+//         message: 'Username does not allow other than alpha numeric chars.',
 //     })
 //     username: string;
 
@@ -38,11 +38,11 @@
 
 //     @IsNotEmpty()
 //     @Matches(passwordRegEx, {
-//       message: `Password must contain Minimum 8 and maximum 20 characters,
+//         message: `Password must contain Minimum 8 and maximum 20 characters,
 //       at least one uppercase letter,
 //       one lowercase letter,
 //       one number and
 //       one special character`,
 //     })
 //     password: string;
-//   }
+// }
