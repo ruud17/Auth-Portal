d780923b2c2b5db1102f49925644aec76ef4591e
Mehmed Jusic <mehmedjusic@gmail.com>
2024-02-20 15:59:00 +0100

    Added missing env vars in docker compose

--- start_docker.sh
+++ start_docker.sh
@@ -10,6 +10,9 @@ export POSTGRES_USER=$DB_USER
 export POSTGRES_PASSWORD=$DB_PASSWORD
 export POSTGRES_DB=$DB_NAME
 export POSTGRES_PORT=$DB_PORT
+export REACT_APP_API_BASE_URL=$REACT_APP_API_BASE_URL
+
+chmod +x init-db.sh
 
 # Now run docker-compose
 docker-compose up
